e57e6dfb8a45649d69bb1539130e72942683c2ae
Yohan THOMAS <ythomas@e3r8p5.42.fr>
2020-03-06 17:26:16 +0100

    frst commit + add an existing module (power)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 with open('README.md', 'rb') as f:
     DESCRIPTION = f.read().decode('utf-8')
 
-with open('timeflux_example/__init__.py') as f:
+with open('app-module/__init__.py') as f:
     VERSION = re.search('^__version__\s*=\s*\'(.*)\'', f.read(), re.M).group(1)
 
 DEPENDENCIES = [
@@ -14,12 +14,12 @@ DEPENDENCIES = [
 ]
 
 setup(
-    name='timeflux-example',
+    name='brain_game_ressources',
     packages=find_packages(),
     version=VERSION,
-    description='An example Timeflux plugin.',
+    description='Needed features for Brain_game',
     long_description=DESCRIPTION,
-    author='Pierre Clisson',
+    author='Thomas yohan',
     author_email='contact@timeflux.io',
     url='https://timeflux.io',
     install_requires=DEPENDENCIES
