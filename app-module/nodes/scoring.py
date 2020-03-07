9c95e08a89ed4ac5f8d664848bb7d84dc82a6cc4
Yohan THOMAS <ythomas@e3r11p9.42.fr>
2020-03-07 16:14:09 +0100

    test

--- app-module/nodes/scoring.py
+++ app-module/nodes/scoring.py
@@ -12,11 +12,11 @@ class Scoring(Node):
     """
     def __init__(self, w_delta=0., w_theta=0., w_alpha=0., w_beta=0., w_gamma=0.):
         """ Weight for each waves """
-        self.w_delta = w_delta   "(float)"
-        self.w_theta = w_theta   "(float)"
-        self.w_alpha = w_alpha   "(float)"
-        self.w_beta = w_beta     "(float)"
-        self.w_gamma = w_gamma   "(float)"
+        self.w_delta = w_delta   #"(float)"
+        self.w_theta = w_theta   #"(float)"
+        self.w_alpha = w_alpha   #"(float)"
+        self.w_beta = w_beta     #"(float)"
+        self.w_gamma = w_gamma   #"(float)"
 
     def update(self):
         if not self.i.ready():
